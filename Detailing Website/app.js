0cc0e5237d60747637ead0a3bcc4358816ecdb73
Nola360 <40280292+Nola360@users.noreply.github.com>
2023-02-27 21:30:27 -0500

    Add files via upload

---
+++ Detailing Website/app.js
@@ -0,0 +1,26 @@
+// window.addEventListener('scroll', (e) => {
+//   const info_section = document.querySelectorAll('.info_content')
+//   const scrollY = window.scrollY
+
+
+
+//   info_section.forEach(section => {
+//     console.log(e)
+//     if (scrollY > 800 && scrollY <= 1400) {
+//       console.log("first wave effect");
+//       section.classList.add('active');
+//     } else if (scrollY > 1400 && scrollY < 2183) {
+//       console.log('second wave effect');
+//       section.classList.add('active');
+//     } else if (scrollY >= 2183) {
+//       console.log("third wave effect");
+//       section.classList.add('active');
+//     } else {
+//       section.classList.remove('active');
+//       // console.log('remove');
+//     }
+//   })
+
+// })
+
+
